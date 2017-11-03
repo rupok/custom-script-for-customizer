7a58ee6058fdbd7170b5761fdb63fd6cea73009c
Nazmul Hasan Rupok <re.enter.rupok@gmail.com>
2017-11-03 12:34:18 +0600

    public assers removed

--- includes/class-custom-script-for-customizer.php
+++ includes/class-custom-script-for-customizer.php
@@ -77,7 +77,6 @@ class Custom_Script_For_Customizer {
 		$this->load_dependencies();
 		$this->set_locale();
 		$this->define_admin_hooks();
-		$this->define_public_hooks();
 
 	}
 
@@ -89,7 +88,6 @@ class Custom_Script_For_Customizer {
 	 * - Custom_Script_For_Customizer_Loader. Orchestrates the hooks of the plugin.
 	 * - Custom_Script_For_Customizer_i18n. Defines internationalization functionality.
 	 * - Custom_Script_For_Customizer_Admin. Defines all hooks for the admin area.
-	 * - Custom_Script_For_Customizer_Public. Defines all hooks for the public side of the site.
 	 *
 	 * Create an instance of the loader which will be used to register the hooks
 	 * with WordPress.
@@ -115,12 +113,6 @@ class Custom_Script_For_Customizer {
 		 * The class responsible for defining all actions that occur in the admin area.
 		 */
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'admin/class-custom-script-for-customizer-admin.php';
-
-		/**
-		 * The class responsible for defining all actions that occur in the public-facing
-		 * side of the site.
-		 */
-		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/class-custom-script-for-customizer-public.php';
 
 		$this->loader = new Custom_Script_For_Customizer_Loader();
 
@@ -159,21 +151,6 @@ class Custom_Script_For_Customizer {
 
 	}
 
-	/**
-	 * Register all of the hooks related to the public-facing functionality
-	 * of the plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 */
-	private function define_public_hooks() {
-
-		$plugin_public = new Custom_Script_For_Customizer_Public( $this->get_plugin_name(), $this->get_version() );
-
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
-		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
-
-	}
 
 	/**
 	 * Run the loader to execute all of the hooks with WordPress.
